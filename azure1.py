4fe8e644e6caa6bd6abfcf337851cf07203821fb
smskero <122059426+smskero@users.noreply.github.com>
2023-12-08 15:12:04 -0500

    Update azure1.py
    Add instructions (subhead)

--- azure1.py
+++ azure1.py
@@ -24,6 +24,7 @@ openai_api_key = os.environ.get('OPENAI_API_KEY')
 
 # Initialize Streamlit
 st.title("LLM Chatbot")
+st.subheader("Instructions")
 
 # Create a text input field for user queries
 user_input = st.text_input("Ask a question:")
