a8c569bd22037b3d33e9ca1ca161748e18c63180
dipankarduttachowdhury <121707836+dipankarduttachowdhury@users.noreply.github.com>
2023-12-10 16:53:19 -0500

    Update azure_new.py

--- azure_new.py
+++ azure_new.py
@@ -58,9 +58,78 @@ def generate_response(query, mode):
     
     if mode == 'Expert':
         # Generate response in formal legal tone
-        response = result  # Replace this with expert-level response logic
+        response = expert_prompt.generate_prompt(input=query).get_template()
     else:
         # Generate response in simpler language
-        response = result  # Replace this with novice-level response logic
-    
+        response = novice_prompt.generate_prompt(input=query).get_template()
     return response
+
+from langchain.prompts import PromptTemplate
+
+expert_template = """You are a lawyer for the City of Boston and a legal expert.
+You are great at answering questions about Boston Municipal codes using leagl jargons.
+However, you don't have the ability to answer in an easy to understand language.
+
+Here is a question:
+{input}"""
+
+expert_prompt = PromptTemplate.from_template(expert_template)
+
+novice_template = """You are a lawyer for the City of Boston and a legal expert.
+You have a special talent for answering questions in a very easy language so that even laypersons without a background in law can understand the responses.
+You never use complex legal words or jargon to give answer to questions.
+
+Here is a question:
+{input}"""
+
+novice_prompt = PromptTemplate.from_template(novice_template)
+
+routes = [
+    {
+        "name": "expert",
+        "description": "Give expert responses using complex legal jargon if the mode selected is Expert",
+        "prompt_template": expert_template
+    },
+    {
+        "name": "novice",
+        "description": "Give legal responses in a very easy to understand language if the mode selected is Novice",
+        "prompt_template": novice_template
+    }
+]
+
+llm = OpenAI()
+
+from langchain.chains import ConversationChain
+from langchain.chains.llm import LLMChain
+from langchain.chains.router import MultiPromptChain
+from langchain.llms import OpenAI
+from langchain import PromptTemplate
+
+destination_chains = {}
+for p_info in routes:
+    name = p_info["name"]
+    prompt_template = p_info["prompt_template"]
+    prompt = PromptTemplate(template=prompt_template, input_variables=["input"])
+    chain = LLMChain(llm=llm, prompt=prompt)
+    destination_chains[name] = chain
+default_chain = ConversationChain(llm=llm, output_key="text")
+
+from langchain.chains.router.llm_router import LLMRouterChain, RouterOutputParser
+from langchain.chains.router.multi_prompt_prompt import MULTI_PROMPT_ROUTER_TEMPLATE
+
+destinations = [f"{p['name']}: {p['description']}" for p in routes]
+destinations_str = "\n".join(destinations)
+router_template = MULTI_PROMPT_ROUTER_TEMPLATE.format(destinations=destinations_str)
+router_prompt = PromptTemplate(
+    template=router_template,
+    input_variables=["input"],
+    output_parser=RouterOutputParser(),
+)
+router_chain = LLMRouterChain.from_llm(llm, router_prompt)
+
+chain = MultiPromptChain(
+    router_chain=router_chain,
+    destination_chains=destination_chains,
+    default_chain=default_chain,
+    verbose=True,
+)
